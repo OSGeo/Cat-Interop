46013f9734cf6ed73067f9c202847a900364629e
Tom Kralidis <tomkralidis@gmail.com>
2014-02-27 12:38:39 -0500

    add link types proposal

---
+++ link_types_dumps.py
@@ -0,0 +1,23 @@
+# script to serialize link type lists to JSON or XML
+import csv
+import json
+
+
+def to_json(csvobj):
+    """serialize to JSON"""
+
+    # skip CSV header
+    csvobj.next()
+    payload = []
+    for row in csvobj:
+        payload.append({'link_type': row[0], 'description': row[1]})
+
+    return json.dumps(payload)
+
+# main
+
+with open('link_types.csv') as csvfile:
+    CSV_READER = csv.reader(csvfile)
+    PAYLOAD = to_json(CSV_READER)
+
+print PAYLOAD
